845dc0b4e29f77e26f36088a44de4161b2163f49
Henrik Olsson <henrik.olsson@valtech.com>
2022-07-04 05:36:32 +0200

    Refactor

--- HumanDateTime/HumanDateTime.cs
+++ HumanDateTime/HumanDateTime.cs
@@ -4,71 +4,77 @@ public static class HumanDateTime
 {
     public static string ToHumanString(this DateTime dateTime)
     {
+        var datePart = GetDatePart(dateTime);
+        var timePart = GetTimePart(dateTime);
+        if (datePart != "")
+            datePart += " ";
+        return datePart + timePart;
+    }
 
+    private static string GetTimePart(DateTime dateTime)
+    {
         var diff = dateTime - CurrentDateTimeProvider();
         var past = diff.Ticks <= 0;
         var diffInMinutes = Math.Abs(Math.Round(diff.TotalMinutes));
-        var diffInDays = Math.Abs((dateTime.Date - CurrentDateTimeProvider().Date).TotalDays);
-        string datePart;
-        string timePart;
-
-        if (past)
-        {
-            timePart = diffInMinutes switch
+        return past
+            ? diffInMinutes switch
             {
                 0 => "nyss",
                 1 => "1 minut sedan",
                 < 60 => diffInMinutes + " minuter sedan",
                 _ => dateTime.TimeOfDay.ToString(TimeSpanFormat)
-            };
-            datePart = diffInDays switch
-            {
-                0 => "",
-                1 => "i går ",
-                2 => "i förrgår ",
-                < 7 => dateTime.DayOfWeek switch
-                {
-                    DayOfWeek.Sunday => "i söndags ",
-                    DayOfWeek.Monday => "i måndags ",
-                    DayOfWeek.Tuesday => "i tisdags ",
-                    DayOfWeek.Wednesday => "i onsdags ",
-                    DayOfWeek.Thursday => "i torsdags ",
-                    DayOfWeek.Friday => "i fredags ",
-                    DayOfWeek.Saturday => "i lördags ",
-                    _ => throw new ArgumentOutOfRangeException(nameof(dateTime), $"Unkown day of week \"{dateTime.DayOfWeek}\""),
-                },
-                _ => dateTime.ToString(DateFormat) + " "
-            };
-        }
-        else
-        {
-            timePart = diffInMinutes switch
+            }
+            : diffInMinutes switch
             {
                 0 => "straxt",
                 1 => "om 1 minut",
                 < 60 => "om " + diffInMinutes + " minuter",
                 _ => dateTime.TimeOfDay.ToString(TimeSpanFormat)
             };
-            datePart = diffInDays switch
+    }
+
+    private static string GetDatePart(DateTime dateTime)
+    {
+        var diff = dateTime - CurrentDateTimeProvider();
+        var past = diff.Ticks <= 0;
+        var diffInDays = Math.Abs((dateTime.Date - CurrentDateTimeProvider().Date).TotalDays);
+        return past
+            ? diffInDays switch
             {
                 0 => "",
-                1 => "i morgon ",
-                2 => "i övermorgon ",
+                1 => "i går",
+                2 => "i förrgår",
                 < 7 => dateTime.DayOfWeek switch
                 {
-                    DayOfWeek.Sunday => "på söndag ",
-                    DayOfWeek.Monday => "på måndag ",
-                    DayOfWeek.Tuesday => "på tisdag ",
-                    DayOfWeek.Wednesday => "på onsdag ",
-                    DayOfWeek.Thursday => "på torsdag ",
-                    DayOfWeek.Friday => "på fredag ",
-                    DayOfWeek.Saturday => "på lördag ",
+                    DayOfWeek.Sunday => "i söndags",
+                    DayOfWeek.Monday => "i måndags",
+                    DayOfWeek.Tuesday => "i tisdags",
+                    DayOfWeek.Wednesday => "i onsdags",
+                    DayOfWeek.Thursday => "i torsdags",
+                    DayOfWeek.Friday => "i fredags",
+                    DayOfWeek.Saturday => "i lördags",
                     _ => throw new ArgumentOutOfRangeException(nameof(dateTime), $"Unkown day of week \"{dateTime.DayOfWeek}\""),
                 },
-                _ => dateTime.ToString(DateFormat) + " "
+                _ => dateTime.ToString(DateFormat)
+            }
+            : diffInDays switch
+            {
+                0 => "",
+                1 => "i morgon",
+                2 => "i övermorgon",
+                < 7 => dateTime.DayOfWeek switch
+                {
+                    DayOfWeek.Sunday => "på söndag",
+                    DayOfWeek.Monday => "på måndag",
+                    DayOfWeek.Tuesday => "på tisdag",
+                    DayOfWeek.Wednesday => "på onsdag",
+                    DayOfWeek.Thursday => "på torsdag",
+                    DayOfWeek.Friday => "på fredag",
+                    DayOfWeek.Saturday => "på lördag",
+                    _ => throw new ArgumentOutOfRangeException(nameof(dateTime), $"Unkown day of week \"{dateTime.DayOfWeek}\""),
+                },
+                _ => dateTime.ToString(DateFormat)
             };
-        }
-        return datePart + timePart;
     }
 
     internal static Func<DateTime> CurrentDateTimeProvider = () => DateTime.Now; // Set to constant in tests.
