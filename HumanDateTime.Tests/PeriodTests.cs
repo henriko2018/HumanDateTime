2410d71e8af5c8b60ffe7f8509a5afad9960c530
Henrik Olsson <u.henrik.olsson@live.se>
2022-07-07 16:55:59 +0200

    Add support for nullable DateTime

--- HumanDateTime.Tests/PeriodTests.cs
+++ HumanDateTime.Tests/PeriodTests.cs
@@ -9,10 +9,24 @@ public class PeriodTests
     public void OutputsExpected(DateTime from, DateTime to, string expected)
         => Assert.Equal(expected, HumanDateTime.ToHumanString(from, to));
 
+    [Theory]
+    [MemberData(nameof(NullableTestData))]
+    public void NullableOutputsExpected(DateTime? from, DateTime? to, string expected)
+        => Assert.Equal(expected, HumanDateTime.ToHumanString(from, to));
+
     public static IEnumerable<object[]> TestData =>
         new List<object[]>
         {
             new object[] { new DateTime(2022, 06, 01, 08, 00, 00), new DateTime(2022, 06, 01, 17, 00, 00), "i dag 08:00-17:00" },
             new object[] { new DateTime(2022, 06, 01, 08, 00, 00), new DateTime(2022, 06, 02, 17, 00, 00), "i dag 08:00 - i morgon 17:00"}
+        };
+
+    public static IEnumerable<object?[]> NullableTestData =>
+        new List<object?[]>
+        {
+            new object?[] { new DateTime?(new DateTime(2022, 06, 01, 08, 00, 00)), new DateTime?(new DateTime(2022, 06, 01, 17, 00, 00)), "i dag 08:00-17:00" },
+            new object?[] { new DateTime?(new DateTime(2022, 06, 01, 08, 00, 00)), new DateTime?(new DateTime(2022, 06, 02, 17, 00, 00)), "i dag 08:00 - i morgon 17:00"},
+            new object?[] { new DateTime?(new DateTime(2022, 06, 01, 08, 00, 00)), null, "i dag 08:00 -"},
+            new object?[] { null, new DateTime?(new DateTime(2022, 06, 02, 17, 00, 00)), "- i morgon 17:00"},
         };
 }
